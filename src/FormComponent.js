ac100a47d2b48b0a985efadb945c8be0928111d6
Dorennor <vova.rud.00@gmail.com>
2020-12-11 02:14:52 +0200

    0.1

---
+++ src/FormComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FormComponent() {
+    return (
+        <form action="">
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <button>Enter</button>
+        </form>
+    )
+}
+
+export default FormComponent
