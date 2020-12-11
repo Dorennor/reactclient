e15eb40149015612b70692a9ed313fc0f835e363
Dorennor <vova.rud.00@gmail.com>
2020-12-11 04:04:38 +0200

    0.1.2

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './LoginForm.css';
 
 function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    axios
+      .post('/user', {
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="login-box">
       <h2>Login</h2>
       <form>
-        <div className="user-box">
-          <input type="text" name="" required="" />
-          <label>Username</label>
+        <div className="email-box">
+          <input
+            type="email"
+            name=""
+            required=""
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <label>Email</label>
         </div>
-        <div className="user-box">
-          <input type="password" name="" required="" />
+        <div className="email-box">
+          <input
+            type="password"
+            name=""
+            required=""
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
           <label>Password</label>
         </div>
-        <a href="#">
+        <a href="#" onClick={handleSubmit}>
           <span></span>
           <span></span>
           <span></span>
